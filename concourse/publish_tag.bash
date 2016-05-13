79a0217938edd9861bd44b5fe0572bb345e0d986
Jesse Zhang and Xin Zhang <jzhang@pivotal.io>
2016-05-13 10:46:55 -0700

    WIP: Fix up the output

--- concourse/publish_tag.bash
+++ concourse/publish_tag.bash
@@ -3,9 +3,9 @@
 set -u -e -x
 
 main() {
-  echo "test release" > /tmp/name.txt
-  echo "v0.1" > /tmp/tag.txt
-  echo "hello" > /tmp/body.txt
+  echo "test release" > gpos_github_release_stage/name.txt
+  echo "v0.1" > gpos_github_release_stage/tag.txt
+  echo "hello" > gpos_github_release_stage/body.txt
 }
 
 main "$@"
