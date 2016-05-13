9483616d467235f70da160abe5ef44a2a2c1d80b
Jesse Zhang and Xin Zhang <xzhang@pivotal.io>
2016-05-13 14:28:49 -0700

    WIP: Figure out proper version number

--- concourse/publish_tag.bash
+++ concourse/publish_tag.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+set -o pipefail
 set -u -e -x
 
 main() {
-  echo "test release" > gpos_github_release_stage/name.txt
-  echo "v0.1" > gpos_github_release_stage/tag.txt
-  echo "hello" > gpos_github_release_stage/body.txt
+  env LC_ALL=C tar tf bin_gpos_centos5_release/bin_gpos_centos5_release.tar.gz | grep "libgpos.so." | sort -n | head -n 1 | sed 's/\.\/lib\/libgpos\.so\./v/' > gpos_github_release_stage/tag.txt
+  env GIT_DIR=gpos_src/.git git rev-parse HEAD > gpos_github_release_stage/commit.txt
 }
 
 main "$@"
