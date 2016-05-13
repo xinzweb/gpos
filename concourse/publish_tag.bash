ecc871e6f547cddd62f1ca65e3e98b5635bbe00d
HaiSheng Yuan and Xin Zhang <xzhang@pivotal.io>
2016-05-12 18:22:24 -0700

    WIP: hardcode output directory

--- concourse/publish_tag.bash
+++ concourse/publish_tag.bash
@@ -3,10 +3,9 @@
 set -u -e -x
 
 main() {
-  pushd gpos_src
-  echo "test release" > /tmp/build/put/gpos_src/name.txt
-  echo "v0.1" > /tmp/build/put/gpos_src/tag.txt
-  echo "hello" > /tmp/build/put/gpos_src/body.txt
+  echo "test release" > /tmp/name.txt
+  echo "v0.1" > /tmp/tag.txt
+  echo "hello" > /tmp/body.txt
 }
 
 main "$@"
